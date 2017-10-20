fb6302b4142ba603e2eb8435a9f5e18d836264b4
yacin92 <yacincheffai@yahoo.com>
2017-10-20 18:08:19 +0100

    'fixing_statics'

--- jobs/urls.py
+++ jobs/urls.py
@@ -21,7 +21,3 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('jobs_search.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
