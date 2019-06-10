17fae8ddca8325766eb115b08c13443d1023d80a
yacincheffai <y.cheffai-ext@rg2i.net>
2019-06-10 12:23:46 +0100

    [TV-015][TV-017]IMP use postres

--- jobs/settings.py
+++ jobs/settings.py
@@ -76,7 +76,18 @@ WSGI_APPLICATION = 'jobs.wsgi.application'
 
 default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
-DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
+# DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'postgres',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
